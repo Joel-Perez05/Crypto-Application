37945a5824d0d3039c11c59813c942805269b1a8
Joel Perez <joel_perez65@yahoo.com>
2024-01-14 23:20:44 -0600

    finished line graph on bottom of individual coin page.

--- src/app/coin/[id]/page.tsx
+++ src/app/coin/[id]/page.tsx
@@ -6,11 +6,12 @@ import CoinAtlAthInfo from "@/app/components/CoinAtlAthInfo";
 import CoinMarketInfo from "@/app/components/CoinMarketInfo";
 import CoinDescription from "@/app/components/CoinDescription";
 import CoinConvertor from "@/app/components/CoinConvertor";
+import LineGraphCoinPage from "@/app/components/LineGraphCoinPage";
 
 export type CoinType = {
   id: string;
   symbol?: string;
-  name: string;
+  name?: string;
   image: {
     thumb: string;
     small: string;
@@ -70,48 +71,66 @@ type CoinPageProps = {
   };
 };
 
+export type CoinPriceType = {
+  prices?: Array<[number, number]>;
+};
+
 const CoinPage: React.FC<CoinPageProps> = ({ params }) => {
   const [coin, setCoin] = useState<CoinType>();
+  const [coinPrice, setCoinPrice] = useState<CoinPriceType>();
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await axios.get(
-          `https://api.coingecko.com/api/v3/coins/${params.id}?localization=false&tickers=false&market_data=true&community_data=true&developer_data=false&sparkline=false`
-        );
+    axios
+      .get(
+        `https://api.coingecko.com/api/v3/coins/${params.id}?localization=false&tickers=false&market_data=true&community_data=true&developer_data=false&sparkline=false`
+      )
+      .then((res) => {
         setCoin(res.data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    if (typeof window !== "undefined") {
-      fetchData();
-    }
+      })
+      .catch((err) => err);
+    axios
+      .get(
+        `https://api.coingecko.com/api/v3/coins/${params.id}/market_chart?vs_currency=usd&days=180&interval=daily`
+      )
+      .then((res) => {
+        setCoinPrice(res.data);
+      })
+      .catch((err) => err);
   }, [params.id]);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="h-full md:w-full xl:w-1/2 p-4 bg-custom-dark1">
-        <h2 className="text-white text-3xl">Your Summary:</h2>
-        <div className="mt-6 flex justify-around">
-          <CoinMainInfo
+    <div>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <div className="h-full md:w-full xl:w-1/2 p-4 bg-custom-dark1">
+          <h2 className="text-white text-3xl">Your Summary:</h2>
+          <div className="mt-6 flex justify-around">
+            <CoinMainInfo
+              links={coin?.links}
+              image={coin?.image}
+              symbol={coin?.symbol}
+              name={coin?.name}
+            />
+            <CoinAtlAthInfo market_data={coin?.market_data} />
+            <CoinMarketInfo
+              symbol={coin?.symbol}
+              market_data={coin?.market_data}
+            />
+          </div>
+          <h2 className="text-white text-3xl mt-6">Description:</h2>
+          <CoinDescription
+            description={coin?.description}
             links={coin?.links}
-            image={coin?.image}
-            symbol={coin?.symbol}
-            name={coin?.name}
           />
-          <CoinAtlAthInfo market_data={coin?.market_data} />
-          <CoinMarketInfo
+          <CoinConvertor
             symbol={coin?.symbol}
             market_data={coin?.market_data}
           />
         </div>
-        <h2 className="text-white text-3xl mt-6">Description:</h2>
-        <CoinDescription description={coin?.description} links={coin?.links} />
-        <CoinConvertor symbol={coin?.symbol} market_data={coin?.market_data} />
+      </main>
+      <div className="-mt-24">
+        <LineGraphCoinPage prices={coinPrice?.prices} />
       </div>
-    </main>
+    </div>
   );
 };
 
